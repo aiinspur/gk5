ac3fc5526a74e2b9a88b381ba9a855dd6614c6de
shihujiang <jiangshihu@hotmail.com>
2017-03-12 01:30:12 +0800

    init

---
+++ src/test/java/com/tigerj/Gk5ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tigerj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Gk5ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
